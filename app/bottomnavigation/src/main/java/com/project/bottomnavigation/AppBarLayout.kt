72d3d6f99a59e467863b0d111b07298064f5ca25
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-13 22:17:21 +0530

    Initial commit for codelabs

--- app/bottomnavigation/src/main/java/com/project/bottomnavigation/AppBarLayout.kt
+++ app/bottomnavigation/src/main/java/com/project/bottomnavigation/AppBarLayout.kt
@@ -2,10 +2,38 @@ package com.project.bottomnavigation
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class AppBarLayout:AppCompatActivity() {
+class AppBarLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_app_bar_layout)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                Toast.makeText(this, "This is Home Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.cart -> {
+                Toast.makeText(this, "This is Cart Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.setting -> {
+                Toast.makeText(this, "This is Setting Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+                return true
+            }
+        }
+
+    }
+
 }
