891ae002883ca06e1a0eb6182b490ac49165f481
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-06-13 20:11:56 +0530

    Initial commit for codelabs

---
+++ app/bottomnavigation/src/main/java/com/project/bottomnavigation/AppBarLayout.kt
@@ -0,0 +1,11 @@
+package com.project.bottomnavigation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AppBarLayout:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
